acdd9b222d204d7767cd6c6e0ffcde5347b83e07
dvaqueiro <dvaqueiro@boardfy.com>
2019-07-03 21:14:07 +0200

    improvements

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# vim configuration
+ln -s ~/dev/dotfiles/vim ~/.vim
+
 echo "Installing vim plug";
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
