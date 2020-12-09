acaadf1774564df9134454c407d315da25f70246
dvaqueiro <dvaqueiro>
2020-12-09 22:14:27 +0100

    mysql database manage scripts parametrize

--- bin/mysql_backup.sh
+++ bin/mysql_backup.sh
@@ -4,26 +4,23 @@
 # Copyright (c) 2020 Daniel Vaqueiro <dvaqueiro@boardfy.com>
 # This script is licensed under GNU GPL version 2.0 or above
 # -------------------------------------------------------------------------
-### SETUP MYSQL LOGIN ###
-MCFGFILE='config.cnf'
-
-### Set to 1 if you need to see progress while dumping dbs ###
-VERBOSE=1
-EXCLUDED_DBS='information_schema performance_schema sys'
-
-### Setup dump local directory ###
-BASEDIR='/tmp/mysql_backups'
-DST_HOST='admin@storage'
-DST_SSH_PORT='2222'
-REMOTE_BASE_DIR='/tmp/mysql_backups'
-BACKUP_RETENTION=3
-
-### Default time format ###
-TIME_FORMAT='%H_%M_%S'
-
 #####################################
 ### ----[ No Editing below ]------###
 #####################################
+### SETUP MYSQL LOGIN ###
+MCFGFILE='config.cnf'
+### Set to 1 if you need to see progress while dumping dbs ###
+VERBOSE=0
+EXCLUDED_DBS='information_schema performance_schema sys'
+### Setup dump local directory ###
+BAKRSNROOT='/tmp/mysql_backups'
+DST_HOST='user@storage.somedomain.com'
+DST_SSH_PORT='22'
+REMOTE_DIR='/tmp/mysql_backups'
+BACKUP_RETENTION=3
+### Default time format ###
+TIME_FORMAT='%Y_%m_%d-%H_%M_%S'
+
 ### Set bins path ###
 GZIP=$(command -v gzip)
 MYSQL=$(command -v mysql)
@@ -35,16 +32,13 @@ GREP=$(command -v grep)
 SSH=$(command -v ssh)
 RSYNC=$(command -v rsync)
 
-HNAME=$(hostname)
-BAKRSNROOT="$BASEDIR/$HNAME"
-REMOTE_DIR="$REMOTE_BASE_DIR/$HNAME"
 
 MYSQLDUMPFLAGS='--max-allowed-packet=128000000'
 RSYNCFLAGS='-avh  --remove-source-files'
 
 ### Die on demand with message ###
 die(){
-    echo "$@"
+    echo "[Fatal error] $@"
     exit 999
 }
 
@@ -75,7 +69,6 @@ perform_backups(){
     local db=""
     [ ! -d $BAKRSNROOT ] && ${MKDIR} -p $BAKRSNROOT
     ssh -t -t -p $DST_SSH_PORT $DST_HOST "[ ! -d $REMOTE_DIR ] && ${MKDIR} -p $REMOTE_DIR"
-    ${RM} -f $BAKRSNROOT/* >/dev/null 2>&1
     [ $VERBOSE -eq 1 ] && echo -e "\n*** Dumping MySQL Database ***"
     for db in $DBS
     do
@@ -85,7 +78,7 @@ perform_backups(){
             continue
         fi
         local tTime=$(date +"${TIME_FORMAT}")
-        local FILE="${BAKRSNROOT}/${db}.${tTime}.gz"
+        local FILE="${BAKRSNROOT}/${db}-${tTime}.gz"
         [ $VERBOSE -eq 1 ] && echo "$db.."
         ${MYSQLDUMP} --defaults-extra-file=$MCFGFILE $MYSQLDUMPFLAGS $db | ${GZIP} -9 > $FILE
         [ $VERBOSE -eq 1 ] && echo -e "rsync $FILE to remote storage..\n"
@@ -93,10 +86,56 @@ perform_backups(){
         ssh -t -t -p $DST_SSH_PORT $DST_HOST \
             "cd $REMOTE_DIR ; ls -t | grep $db | sed -e 1,"$BACKUP_RETENTION"d | tr '\n' ' ' | xargs rm -R > /dev/null 2>&1"
     done
-    [ $VERBOSE -eq 1 ] && echo -e "*** Backup done [ files wrote to $BAKRSNROOT ] ***\n"
+    [ $VERBOSE -eq 1 ] && echo -e "*** Backup done [ files transfered to $REMOTE_DIR ] ***\n"
 }
+
+usage()
+{
+  echo -e "Usage: $0 [options...]"
+  echo -e " -h\t\tShow this message and exit."
+  echo -e " -l <path>\tLocal path to temporary dump. Default:/tmp/mysql_backups"
+  echo -e " -t <path>\tRemote path to store backup. Default:/tmp/mysql_backups"
+  echo -e " -d <host>\tRemote host. Default:user@storage.somedomain.com"
+  echo -e " -p <port>\tRemote ssh host port. Default:22"
+  echo -e " -o <format>\tTime format. Default:%H_%M_%S"
+  echo -e " -f <file>\tMysql connection config file. Default:config.cnf"
+  echo -e " -r <number>\tBackup retention. Default:3"
+  echo -e " -e <dbs>\tExcluded databases. Default:information_schema performance_schema sys"
+  echo -e " -v\t\tVerbose."
+  exit 2
+}
+
+show_config() {
+    [ $VERBOSE -eq 1 ] && echo "MCFGFILE: $MCFGFILE"
+    [ $VERBOSE -eq 1 ] && echo "VERBOSE: $VERBOSE"
+    [ $VERBOSE -eq 1 ] && echo "EXCLUDED_DBS: $EXCLUDED_DBS"
+    [ $VERBOSE -eq 1 ] && echo "BASEDIR: $BAKRSNROOT"
+    [ $VERBOSE -eq 1 ] && echo "DST_HOST: $DST_HOST"
+    [ $VERBOSE -eq 1 ] && echo "DST_SSH_PORT: $DST_SSH_PORT"
+    [ $VERBOSE -eq 1 ] && echo "REMOTE_DIR: $REMOTE_DIR"
+    [ $VERBOSE -eq 1 ] && echo "BACKUP_RETENTION: $BACKUP_RETENTION"
+    [ $VERBOSE -eq 1 ] && echo "TIME_FORMAT: $TIME_FORMAT"
+}
+
+while getopts 'vhl:r:d:p:t:f:r:e:' c
+do
+    case $c in
+        v) VERBOSE=1 ;;
+        l) BAKRSNROOT=$OPTARG ;;
+        t) REMOTE_DIR=$OPTARG ;;
+        d) DST_HOST=$OPTARG ;;
+        p) DST_SSH_PORT=$OPTARG ;;
+        o) TIME_FORMAT=$OPTARG ;;
+        f) MCFGFILE=$OPTARG ;;
+        r) BACKUP_RETENTION=$OPTARG ;;
+        e) EXCLUDED_DBS=$OPTARG ;;
+        h) usage ;;
+        *) usage ;;
+    esac
+done
 
 ### main ####
 verify_bins
 verify_mysql_connection
+# show_config
 perform_backups
