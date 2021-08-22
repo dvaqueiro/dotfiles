553c4b438c42b0e6d3eed68e4d5c970cf5ce437a
dvaqueiro <dvaqueiro>
2021-08-22 13:22:05 +0200

    add small readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# Installation
+
+```
+git clone git@github.com:dvaqueiro/dotfiles.git ~/.dotfiles
+cd ~/.dotfiles && bash install.sh
+```
--- install.sh
+++ install.sh
@@ -25,8 +25,8 @@ installOhMyZsh() {
 
 installOhMyZshPlugins() {
   printGreenLine "Installing oh-my-zsh-plugins..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 }
 
 installFzf() {
